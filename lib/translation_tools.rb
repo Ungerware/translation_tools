aad31c93e44ba0b1d705fccd79bcc01fd0e1c194
Kaleb Lape <kaleb.lape@gmail.com>
2024-09-24 10:47:31 -0500

    init

---
+++ lib/translation_tools.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "translation_tools/version"
+
+module TranslationTools
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/translation_tools/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TranslationTools
+  VERSION = "0.1.0"
+end
